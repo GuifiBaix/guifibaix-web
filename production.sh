9fb6d0628c32db19533fad17c83438e2710ea4b5
David García Garzón <voki@canvoki.net>
2013-09-26 08:58:31 +0200

    production.sh: Forcing back to the master branch

--- production.sh
+++ production.sh
@@ -2,7 +2,6 @@
 
 # Publishes on http://guifibaix.coop
 
-git checkout gh-pages && git ftp push ftp://guifibaix.coop/ -u guifibaixcoop@vviudez.com -v -p
-git checkout master
+(git checkout gh-pages && git ftp push ftp://guifibaix.coop/ -u guifibaixcoop@vviudez.com -v -p && false ) || git checkout master
 
 
