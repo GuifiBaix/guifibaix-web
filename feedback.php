c21f578e8b34d323b3571a2f02e05dec0c43e4cb
David García Garzón <voki@canvoki.net>
2014-01-04 08:57:07 +0100

    Using a nicer from addres

--- feedback.php
+++ feedback.php
@@ -1,7 +1,7 @@
 <?php
 
 $debug = 0;
-$from_address = "webmaster@noreply.net";
+$from_address = "noreply@guifibaix.coop";
 // split address in two to avoid mail being scanned by robots
 $to_user  = "equipo";
 $to_domain = "guifibaix.coop";
