cd1cea17f41148e65fdc0688515d06519db6ccdb
David García Garzón <voki@canvoki.net>
2015-12-02 10:39:10 +0100

    documenting dependencies in ubuntu

--- production.sh
+++ production.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Publishes on http://guifibaix.coop
+# sudo apt-get install git-ftp
 
 (git checkout gh-pages && git ftp push ftp://guifibaix.coop/ -u guifibaixcoop@vviudez.com -v -p && false ) || git checkout master
 
