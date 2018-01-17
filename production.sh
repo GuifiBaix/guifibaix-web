fcee999aa42291e7a5d4434cfb03c7725fa458a9
David García Garzón <voki@canvoki.net>
2018-01-17 12:11:28 +0100

    using --remote-root option

--- production.sh
+++ production.sh
@@ -3,6 +3,6 @@
 # Publishes on http://guifibaix.coop
 # sudo apt-get install git-ftp
 
-(git checkout gh-pages && git ftp push ftp://guifibaix.coop:site -u guifibaixcoop@vviudez.com -v -P && false ) || git checkout master
+(git checkout gh-pages && git ftp push ftp://guifibaix.coop --remote-root site/ -u guifibaixcoop@vviudez.com -v -P && false ) || git checkout master
 
 
