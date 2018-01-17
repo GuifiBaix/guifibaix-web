0135714a8991128b10cbc3d2557853b764594a48
David García Garzón <voki@canvoki.net>
2018-01-17 19:51:47 +0100

    deployment scripts

--- production.sh
+++ production.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Publishes on http://guifibaix.coop
+
+# Requires gbwebdeploy key pair to be on ~/.ssh
+
+ssh -i ~/.ssh/gbwebdeploy vviudez@guifibaix.coop -p 2222
+
+# Old way, for the record
 # sudo apt-get install git-ftp
 
-(git checkout gh-pages && git ftp push ftp://guifibaix.coop --remote-root site/ -u guifibaixcoop@vviudez.com -v -P && false ) || git checkout master
-
+#(git checkout gh-pages && git ftp push ftp://guifibaix.coop --remote-root site/ -u guifibaixcoop@vviudez.com -v -P && false ) || git checkout master
 
--- publish.sh
+++ publish.sh
@@ -2,7 +2,12 @@
 
 # Publishes on http://guifibaix.github.io/guifibaix-web/
 
+# Just in case something is missing to be generated
 hyde gen
+
+# Merge changes done on deploy/ into the subtree branch in gh-pages
 git subtree split --prefix=deploy --branch gh-pages
+
+# Push gh-pages into github
 git push origin gh-pages
 
