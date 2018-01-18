47304927a9d8b2ba24bb42e822f4f6a5e73b4598
David García Garzón <voki@canvoki.net>
2018-01-18 09:50:22 +0100

    salvaguardes als scripts

--- production.sh
+++ production.sh
@@ -2,6 +2,14 @@
 
 # Publishes on http://guifibaix.coop
 
+
+function die() { echo -e '\033[31;1m'"$@"'\033[0m' >&2; exit -1 }
+
+key=~/.ssh/gbwebdeploy
+
+[ -f "$key" ] || die "Necesitas instalar las claves $(basename $key) en $(dirname $key)"
+(ssh-add -l | grep "$key" >/dev/null) || ssh-add "$key" || die Error cargando la clave gbwebdeploy
+
 # Requires gbwebdeploy key pair to be on ~/.ssh
 ssh -i ~/.ssh/gbwebdeploy vviudez@guifibaix.coop -p 2222
 
--- publish.sh
+++ publish.sh
@@ -2,8 +2,15 @@
 
 # Publishes on http://guifibaix.github.io/guifibaix-web/
 
+
+function die() { echo -e '\033[31m'"$@"'\033[0m' >&2; exit -1; }
+
+
 # Just in case something is missing to be generated
 hyde gen
+
+# Abort if any pending change in content or deploy
+git diff HEAD --exit-code content deploy || die "Error: Per poder publicar, cal comitejar o apartar (git stash) els canvis d'adalt"
 
 # Merge changes done on deploy/ into the subtree branch in gh-pages
 git subtree split --prefix=deploy --branch gh-pages
